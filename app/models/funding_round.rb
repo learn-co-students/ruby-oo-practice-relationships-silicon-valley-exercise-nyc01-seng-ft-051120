dfa3ed4a460628eae5d1657bc5d2bd24b03e2fac
emotionalproof <chaserabenn@gmail.com>
2020-05-15 15:43:48 -0400

    Associations and Aggr methods

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class FundingRound
+
+   attr_accessor :type
+   attr_reader :startup, :venture_capitalist
+
+   @@all = []
+
+   def initialize(startup, venture_capitalist, type, investment)
+      @startup = startup
+      @venture_capitalist = venture_capitalist
+      @type = type
+      @investment = investment.to_f.abs
+      @@all << self
+   end
+
+
 
 end
--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,47 @@
+require 'pry'
+require_relative './funding_round.rb'
+require_relative './venture_capitalist.rb'
+
 class Startup
 
+   attr_accessor :name
+   attr_reader :founder, :domain
+   
+   @@all = []
+
+   def initialize(name, founder, domain)
+      @name = name
+      @founder = founder
+      @domain = domain
+      @@all << self
+   end
+
+   def self.all
+      @@all   
+   end
+
+   def pivot(new_domain, name)
+      @@all.each do |startup|
+         startup.change_domain(new_domain) if startup.name == name
+      end
+   end
+
+
+   def change_domain(new_domain)
+      @domain = new_domain
+   end
+
+   def self.find_by_founder(founder)
+      @@all.find {|company| company.founder == founder }
+   end
+
+   def self.domains
+      @@all.map {|company| company.domain}
+   end
+
+   def sign_contract(venture_capitalist, type, amount)
+      funding_round = FundingRound.new(self, venture_capitalist, type, amount)
+   end
+
+   binding.pry
 end
