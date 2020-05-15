3f174c5d2775a0f86b2b0685d8af6325de04c6ea
ChayaGreisman <chayamgreisman@gmail.com>
2020-05-15 10:44:02 -0700

    first try

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,33 @@
 class Startup
 
+    attr_accessor :name, :domain
+    attr_reader :founder
+
+    @@all = []
+
+    def initialize
+        Startup.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pivot(domain,name)
+        self.domain = domain
+        self.name = name
+    end
+
+    def find_by_founder(founder)
+        Startup.all.find do |startup_instance|
+            startup_instance.founder == founder
+        end
+    end
+
+    def domains
+        Startup.all.map do |startup_instance|
+            startup_instance.domain
+        end
+    end
+
 end
