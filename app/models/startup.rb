7731d5f4319a6608576d1c0cac7e40062b443ed9
callas4ever <thetseringnorbu@gmail.com>
2020-05-15 14:00:23 -0400

    for now

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Startup
+   
+    attr_reader :founder
+    attr_accessor :domain, :name
+    @@all=[]
+    def initialize(name, founder, domain)
+        @name=name
+        @founder=founder
+        @domain=domain
+        @@all << self
 
+    end
+    def all
+        @@all
+    end
+    def pivot(name, domain)
+        @name=name
+        @domain=domain
+    end
+    def find_by_founder(founder)
+        self.all.find do |startup|
+            if startup.founder==founder
+                startup.name
+            end
+        end
+    end
+
+    binding.pry
 end
