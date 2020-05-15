a4e6cc3204c93ec012a88853792af3306923c8a1
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-05-15 13:59:55 -0400

    change 2

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,24 @@
 class VentureCapitalist
+    attr_reader :name
+
+    @@all = []
+
+    def initialize
+        VentureCapitalist.all << self
+    end
+
+    def total_worth
+        total_worth
+    end
+
+    def self.all
+        @@all
+    end
+
+    def tres_commas_club
+        VentureCapitalist.all.map do |venture_capitalist_instance|
+            venture_capitalist_instance.total_worth > 1,000,000,000
+        end
+    end
 
 end
